ba824382d243417289f45ebd5c0300ac1b747588
Yury Oparin <yury.oparin@gmail.com>
2014-05-04 15:47:57 +0400

    Put access token to Redis

--- google_oauth2.sh
+++ google_oauth2.sh
@@ -21,7 +21,6 @@ scope='https://www.googleapis.com/auth/youtube.readonly'
 claim="{\"aud\":\"$aud\",\"exp\":$exp,\"iat\":$iat,\"iss\":\"$iss\",\"scope\":\"$scope\"}"
 
 #echo "exp = $exp"
-#echo "iat = $iat"
 
 header_b64=$(echo -n "$header" | base64 -w 0 | sed 's/+/-/g;s/\//_/g;s/=//g') # base64url
 claim_b64=$(echo -n "$claim" | base64 -w 0 | sed 's/+/-/g;s/\//_/g;s/=//g') # base64url
@@ -33,4 +32,25 @@ jwt=$(echo -n "$header_b64.$claim_b64.$signature_b64")
 result=$(curl -s -m 60 --data-urlencode grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer --data-urlencode assertion=$jwt https://accounts.google.com/o/oauth2/token)
 access_token=$(echo "$result" | grep -oP '"access_token" : "*\K([a-zA-Z0-9\.\-_])*')
 
-echo "access_token = $access_token" # valid for 3600 seconds
+#echo "access_token = $access_token" # valid for 3600 seconds
+
+# The while-loop waits 60 seconds for Redis to start after reboot
+cnt=0
+PING=$(/usr/local/bin/redis-cli PING 2>&1) # stderr to stdout
+
+while [ "$PING" != "PONG" ] && (($cnt < 6)); do
+  sleep 10;
+  let cnt=$cnt+1
+  PING=$(/usr/local/bin/redis-cli PING 2>&1)
+  echo "cnt = $cnt, PING = $PING"
+done
+
+log_date=$(date +'%Y/%m/%d %T')
+
+if [[ $PING == 'PONG' ]]; then
+  # Redis key: google-access-token
+  redis_result=$(/usr/local/bin/redis-cli SET google-access-token "$access_token")
+  echo "[$log_date] access_token = $access_token, exp = $exp, redis SET google-access-token [$redis_result], redis_connection_attempts = $cnt"
+else
+  echo "[$log_date] access_token = $access_token, exp = $exp, REDIS IS NOT UP: $PING"
+fi
